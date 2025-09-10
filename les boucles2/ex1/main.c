aa3c54d9fc1acbfa229d8947d4c0f703ebc1dbd2
anwar962 <elmousaouyanwar31@gmail.com>
2025-09-10 11:39:04 +0200

    code

---
+++ les boucles2/ex1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int i,j;
+printf("x*y  |");
+for(i=10;i>=1;i--)
+    printf("%d" ,i);
+printf("\n");
+{
+for(i=10;i>=1;i--)
+
+    printf("--");
+    printf("\n");s
+    for(i=10;i>=1;i--)
+    {
+        printf("%d  |",i);
+        for(j=1;j>=1;j--)
+        printf("%d",i*j);
+        }
+    printf("\n");
+    }
+    return 0;
+}
---
+++ mini-projet3/mini-projet3/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+    char c[100];
+    char phrase [100];
+    puts("hello,i am feeling happy :");
+    gets(c);
+    while{1}{
+    puts("you:");
+    gets(phrse);
+
+    if(phrase,"exit"=null)
+        puts(bot:goodbye)
+        break;
+    }
+    if(phrase,happy!=null){
+        puts("bot:hi!great to hear that!");
+    }
+
+
+
+    return 0;
+}
